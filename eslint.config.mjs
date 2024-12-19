260a8f66de0926a0039f0adbe73fa7d934a5ae41
Rifat Sarker <rifatswd@gmail.com>
2024-12-19 20:50:01 +0600

    basic setup done

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').L.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "prefer-const": "error",
+      "no-unused-expressions": "error",
+      "no-console": "warn",
+      "no-undef": "error",
+    },
+  },
+  {
+    ignores: [".node_modules/*", "dist"],
+  },
+  { files: ["**/*.{ts}"] },
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import cors from 'cors';
+import express, { Application } from 'express';
+import router from './app/routes';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors({ origin: ['http://localhost:5173'] }));
+
+// application routes
+app.use('/api', router);
+
+export default app;
